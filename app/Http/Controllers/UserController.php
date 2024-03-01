60adf0997fce1625ba0f5d215bf4ba27b273a537
nde95 <ndennison268@gmail.com>
2024-03-01 14:33:41 -0500

    caching leaderboard response for 2 minutes to reduce DB calls

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,17 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class UserController extends Controller
 {
-    public function show(User $user)
-    {
-        return $user->all()->toJson();
-    }
-
     public function leaderboard(User $user)
     {
-        $leaderboard = $user->orderBy('highscore', 'desc')->take(5)->get();
+        $leaderboard = Cache::get('leaderboard');
+
+        if (!$leaderboard) {
+            $leaderboard = $user->orderBy('highscore', 'desc')->take(5)->get();
+            Cache::put('leaderboard', $leaderboard, 120);
+        }
+
         return $leaderboard->toJson();
     }
 }
